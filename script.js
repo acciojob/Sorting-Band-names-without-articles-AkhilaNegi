6c09e239606e15c4fe6bbbb0555395a0e2654459
Akhila Negi <122104289+AkhilaNegi@users.noreply.github.com>
2023-06-23 00:19:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
+//your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-touristSpots.sort((a, b) => {
-    const articles = /^(The|A|An)\s+/i; 
-
-    const spotA = a.replace(articles, '');
-    const spotB = b.replace(articles, '');
-  
-    return spotA.localeCompare(spotB);
-  });
-console.log(touristSpots);
-  
- const ulElement = document.getElementById('band');
-
-
-ulElement.innerHTML = '';
-
-
-touristSpots.forEach((spot) => {
-  const liElement = document.createElement('li');
-  liElement.textContent = spot;
-  ulElement.appendChild(liElement);
+touristSpots.sort((a, b)=>{
+	const func = str => str.replace(/^(a|an|the)\s+/i, '');
+	const modA = func(a);
+	const modB = func(b);
+	return modA.localeCompare(modB);
 });
+
+const ul = document.getElementById('bands');
+for(const spot of touristSpots){
+	const li = document.createElement('li');
+	li.innerHTML = spot;
+	ul.appendChild(li);
+}
